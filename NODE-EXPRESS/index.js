92ff3ebb69eafa205a39dbe4b7139c0d17de639c
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-04-23 17:23:47 +0530

    NODE - EXPRESS

---
+++ NODE-EXPRESS/index.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const http = require('http');
+const morgan=require('morgan')
+const bodyParser = require('body-parser')
+
+const hostname ='localhost';
+const port = 3000;
+const app= express();
+const dishRouter = require('./routes/dishRouter')
+const promoRouter =require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/dishes',dishRouter)
+app.use('/promotions',promoRouter)
+app.use('/leaders',leaderRouter)
+
+app.use(express.static(__dirname+'/public'))
+app.use((req,res,next)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html')
+    res.end('<html><body><h1>EXPRESS SERVER</h1></body></html>')
+    
+});
+
+const server = http.createServer(app);
+server.listen(port,hostname,()=>{
+    console.log(`server runnig at ${hostname}:${port}`)
+})
---
+++ NODE-EXPRESS/routes/leaderRouter.js
@@ -0,0 +1,49 @@
+const express =require('express')
+const bodyParser = require('body-parser')
+
+const leaderRouter = express.Router()
+
+leaderRouter.use(bodyParser.json())
+
+leaderRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leaders: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leaders');
+});
+
+leaderRouter.route('/:leaderId')
+.all((req,res,next)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+
+.get((req,res,next)=>{
+    res.end('will send deatils of the leader'+req.params.leaderId+'to you!!');
+})
+.post((req,res,next)=>{
+    res.statusCode=403;
+    res.end('POST operation not supported on /leader/'+req.params.leaderId)})
+.put((req,res,next)=>{
+    res.write('updating the leader : '+req.params.leaderId +'\n');
+    res.end('will update with :'+req.body.name+'with deatils:'+req.body.description);
+    })
+.delete((req,res,next)=>{
+    res.end('Deleting promo:' + req.params.leaderId);
+})
+
+module.exports= leaderRouter;
