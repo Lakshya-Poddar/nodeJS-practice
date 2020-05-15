f7d324ec69139205d98ee9cd7121c2901dcc0784
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-05-15 09:58:12 +0530

    Update config.js

--- FACEBOOK-AUTH/config.js
+++ FACEBOOK-AUTH/config.js
@@ -2,7 +2,7 @@ module.exports ={
     'secretKey':'12345-67890-09876-54321',
     'mongoUrl':'mongodb://localhost:27017/conFusion',
     'facebook':{
-        clientId:'243559916969056',
-        clientSecret:'df2826aed9ba511eb991f4cb891303ed'
+        clientId:'2#######################6',
+        clientSecret:'d##########################ed'
     }
 }
